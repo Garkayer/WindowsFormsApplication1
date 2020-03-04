59576d3c82405b352ac1812e75090c9673f3dc1e
usis027418 <usis027418@ugb.edu.sv>
2020-03-04 14:28:46 -0600

    Parcial

---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double n1 = double.Parse(txtConversor.Text);
+            double n2 = 8.75;
+            double respuesta;
+            respuesta = n1 * n2;
+            lblRespuesta.Text += "Su Conversiòn de dolar a colon es: " + respuesta;
+          
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double n1 = double.Parse(txtConversor.Text);
+            double n2 = 111.27;
+            double respuesta;
+            respuesta = n1 * n2;
+            lblRespuesta.Text += "Su Conversiòn de dolar a yen es: " + respuesta;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            double n1 = double.Parse(txtConversor.Text);
+            double n2 = 69.75;
+            double respuesta;
+            respuesta = n1 * n2;
+            lblRespuesta.Text += "Su Conversiòn de dolar a rupia es: " + respuesta;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            double n1 = double.Parse(txtConversor.Text);
+            double n2 = 667.08;
+            double respuesta;
+            respuesta = n1 * n2;
+            lblRespuesta.Text += "Su Conversiòn de dolar a peso chileno es: " + respuesta;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            double n1 = double.Parse(txtConversor.Text);
+            double n2 = 19.36;
+            double respuesta;
+            respuesta = n1 * n2;
+            lblRespuesta.Text += "Su Conversiòn de dolar a peso mexicano es: " + respuesta;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            double n1 = double.Parse(txtConversor.Text);
+            double n2 = 39.69;
+            double respuesta;
+            respuesta = n1 * n2;
+            lblRespuesta.Text += "Su Conversiòn de dolar a peso argentino es: " + respuesta;
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            double n1 = double.Parse(txtConversor.Text);
+            double n2 = 0.00026;
+            double respuesta;
+            respuesta = n1 * n2;
+            lblRespuesta.Text += "Su Conversiòn de dolar a bitcoin es: " + respuesta;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ WindowsFormsApplication1/Form2.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double n1 = int.Parse(txtConversor.Text);
+            double n2 = 8.388608;
+            double respuesta;
+            respuesta = n1 * n2;
+            lblRespuesta.Text += "Su Conversiòn de megabyte a bit es: " + respuesta;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double n1 = int.Parse(txtConversor.Text);
+            double n2 = 1.048576;
+            double respuesta;
+            respuesta = n1 * n2;
+            lblRespuesta.Text += "Su Conversiòn de megabyte a byte es: " + respuesta;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            double n1 = int.Parse(txtConversor.Text);
+            double n2 =1024;
+            double respuesta;
+            respuesta = n1 * n2;
+            lblRespuesta.Text += "Su Conversiòn de megabyte a kilobyte es: " + respuesta;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            double n1 = int.Parse(txtConversor.Text);
+            double n2 = 0.0009765625;
+            double respuesta;
+            respuesta = n1 * n2;
+            lblRespuesta.Text += "Su Conversiòn de megabyte a gigabyte es: " + respuesta;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            double n1 = int.Parse(txtConversor.Text);
+            double n2 = 0.00000095367431660625;
+            double respuesta;
+            respuesta = n1 * n2;
+            lblRespuesta.Text += "Su Conversiòn de megabyte a terabyte es: " + respuesta;
+        }
+    }
+}
